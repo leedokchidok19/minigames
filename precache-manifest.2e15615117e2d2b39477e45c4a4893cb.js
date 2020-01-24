661f6a83fdfd3b7f456dd659fd6c2fb4617be3ce
leedokchidok19 <duckhyun4433@naver.com>
2020-01-24 22:44:13 +0900

    Updates

---
+++ precache-manifest.2e15615117e2d2b39477e45c4a4893cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeedbf58833d5079831cf75805930bd0",
+    "url": "/minigames/index.html"
+  },
+  {
+    "revision": "c58c1a54b5f51648c597",
+    "url": "/minigames/static/css/main.b8a4c64a.chunk.css"
+  },
+  {
+    "revision": "1ad835f0a0fc14f9351f",
+    "url": "/minigames/static/js/2.41e4b2c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/minigames/static/js/2.41e4b2c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c58c1a54b5f51648c597",
+    "url": "/minigames/static/js/main.b362f975.chunk.js"
+  },
+  {
+    "revision": "b427ecd23af6246ce39a",
+    "url": "/minigames/static/js/runtime-main.d6bf7404.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/minigames/static/media/logo.5d5d9eef.svg"
+  }
+]);
